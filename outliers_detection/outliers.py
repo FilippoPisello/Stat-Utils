38e81bd78efd9337aa80c38d731901b76cd10818
Filippo Pisello <filippo.pisello@live.it>
2021-11-03 15:08:56 +0100

    feat: function for outliers detection

--- outliers_detection/outliers.py
+++ outliers_detection/outliers.py
@@ -8,6 +8,42 @@ from plot_tools.extras import add_symmetric_interval
 
 import outliers_detection.detectors as detect
 
+
+def outliers_plot(
+    series: Union[pd.Series, np.array],
+    method: str = "mean",
+    factor: int = 2,
+    figsize: tuple[int, int] = (20, 8),
+    lines_color: str = "red",
+) -> None:
+    """Plot the input series denoting the tresholds for outliers.
+
+    Parameters
+    ----------
+    series : Union[pd.Series, np.array]
+        The series to be plotted.
+    method : str, optional
+        The method to be used to identify the outliers, by default "mean".
+    factor : int, optional
+        The factor to be used to determine the accepted range, by default 2.
+    figsize : tuple[int, int], optional
+        The size of the plot, by default (20, 8).
+    lines_color : str, optional
+        The matplotlib color of the lines delimiting the outliers tresholds, by
+        default "red".
+    """
+    detector = _detector_from_method(series, method, factor)
+    _, ax = plot_series(
+        series,
+        figsize,
+        title=f"Outliers plot for {series.name}",
+        return_plot=True,
+    )
+    add_symmetric_interval(
+        ax, detector.center, detector.width, double_variation=False, color=lines_color
+    )
+    plt.show()
+
 def dataframe_outliers(
     dataframe: pd.DataFrame, column: str, method: str = "mean", factor: int = 2
 ) -> pd.DataFrame:

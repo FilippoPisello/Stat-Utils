d21916bd8c15f1d90c63917d4bbc81c0b2197746
Filippo Pisello <filippo.pisello@live.it>
2021-11-13 15:30:32 +0100

    feat: subclasses for binary and numeric prediction

--- predictions/prediction.py
+++ predictions/prediction.py
@@ -109,9 +109,10 @@ class NumericPrediction(Prediction):
             return abs(residuals)
         return residuals
 
-    def matches(self, tolerance: float = 0.0) -> Union[np.ndarray, pd.Series]:
-        """Return a boolean array of length N with True where fitted value is
-        equal to real value."""
+    def matches_tolerance(self, tolerance: float = 0.0) -> Union[np.ndarray, pd.Series]:
+        """Return a boolean array of length N with True where the distance
+        between the real values and the fitted values is inferior to a
+        given parameter."""
         return abs(self.real_values - self.fitted_values) <= tolerance
 
     def as_dataframe(self) -> pd.DataFrame:
@@ -121,7 +122,7 @@ class NumericPrediction(Prediction):
         data = {
             "Fitted Values": self.fitted_values,
             "Real Values": self.real_values,
-            "Prediction Matches": self.matches(),
+            "Prediction Matches": self.matches_tolerance(),
             "Absolute difference": residuals,
             "Relative difference": residuals / self.real_values,
         }
@@ -156,14 +157,17 @@ class BinaryPrediction(Prediction):
         self.value_positive = value_positive
 
     @property
-    def value_negative(self):
+    def value_negative(self) -> Any:
+        """Return the value that it is not the positive value."""
         other_only = self.real_values[self.real_values != self.value_positive]
         if isinstance(self.real_values, np.ndarray):
             return other_only[0].copy()
         return other_only.reset_index(drop=True)[0]
 
     @property
-    def false_positive_rate(self):
+    def false_positive_rate(self) -> float:
+        """Return the ration between the number of wrongly predicted positive
+        and the total number of negatives."""
         pred_pos = self.fitted_values == self.value_positive
         real_neg = self.real_values != self.value_positive
 
@@ -172,6 +176,8 @@ class BinaryPrediction(Prediction):
 
     @property
     def false_negative_rate(self):
+        """Return the ration between the number of wrongly predicted negative
+        and the total number of positives."""
         pred_neg = self.fitted_values != self.value_positive
         real_pos = self.real_values == self.value_positive
 
@@ -180,33 +186,69 @@ class BinaryPrediction(Prediction):
 
     @property
     def sensitivity(self):
+        """Return the ratio between the correctly predicted positive and the
+        total number of real positive."""
         pred_pos = self.fitted_values == self.value_positive
         real_pos = self.real_values == self.value_positive
 
         caught_positive = pred_pos & real_pos
-        return caught_positive.sum() / (pred_pos | real_pos).sum()
+        return caught_positive.sum() / real_pos.sum()
 
     @property
     def specificity(self):
+        """Return the ratio between the correctly predicted negative and the
+        total number of real negative."""
         pred_neg = self.fitted_values != self.value_positive
         real_neg = self.real_values != self.value_positive
 
         caught_negative = pred_neg & real_neg
-        return caught_negative.sum() / (pred_neg | real_neg).sum()
+        return caught_negative.sum() / real_neg.sum()
 
     def confusion_matrix(
-        self, as_dataframe: bool = False
+        self, relative_frequencies: bool = False, as_dataframe: bool = False
     ) -> Union[np.ndarray, pd.DataFrame]:
+        """Return the confusion matrix for the binary classification.
+
+        The confusion matrix is a matrix with shape (2, 2) that classifies the
+        predictions into four categories, each represented by one of its elements:
+        - [0, 0] : negative classified as negative
+        - [0, 1] : negative classified as positive
+        - [1, 0] : positive classified as negative
+        - [1, 1] : positive classified as positive
+
+        Parameters
+        ----------
+        relative_frequencies : bool, optional
+            If True, absolute frequencies are replace by relative frequencies.
+            By default False.
+        as_dataframe : bool, optional
+            If True, the matrix is returned as a pandas dataframe for better
+            readability. Otherwise a numpy array is returned. By default False.
+
+        Returns
+        -------
+        Union[np.ndarray, pd.DataFrame]
+            If as_dataframe is False, return a numpy array of shape (2, 2).
+            Otherwise return a pandas dataframe of the same shape.
+        """
         pred_pos = self.fitted_values == self.value_positive
         pred_neg = self.fitted_values != self.value_positive
         real_pos = self.real_values == self.value_positive
         real_neg = self.real_values != self.value_positive
 
-        conf = np.array(
+        conf_matrix = np.array(
             [
                 [(pred_neg & real_neg).sum(), (pred_pos & real_neg).sum()],
                 [(pred_neg & real_pos).sum(), (pred_pos & real_pos).sum()],
             ]
         )
+
+        # Divide by total number of values to obtain relative frequencies
+        if relative_frequencies:
+            conf_matrix = conf_matrix / len(self.fitted_values)
+
         if not as_dataframe:
-            return conf
+            return conf_matrix
+        conf_df = pd.DataFrame(conf_matrix)
+        conf_df.columns = conf_df.index = [self.value_negative, self.value_positive]
+        return conf_df
--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 import numpy as np
 import pandas as pd
-from predictions.prediction import Prediction
+from predictions.prediction import BinaryPrediction, NumericPrediction, Prediction
 
 
 class TestPrediction(TestCase):
@@ -54,20 +54,19 @@ class TestPrediction(TestCase):
         self.assertTrue(self.pred_s1s2.is_numeric)
         self.assertTrue(Prediction([0.1, 0.2], [0.1, 0.2]).is_numeric)
 
-        self.assertFalse(Prediction(self.l5, None).is_numeric)
-        self.assertFalse(Prediction(np.array(self.l5)).is_numeric)
+        self.assertFalse(Prediction(np.array(self.l5), np.array(self.l5)).is_numeric)
 
     def test_is_correct(self):
         """Test if correctness check happens in the right way."""
         res1 = np.array([True, True, True])
-        np.testing.assert_array_equal(self.pred_l1l2.matches, res1)
-        np.testing.assert_array_equal(self.pred_a1a2.matches, res1)
-        pd.testing.assert_series_equal(self.pred_s1s2.matches, pd.Series(res1))
+        np.testing.assert_array_equal(self.pred_l1l2.matches(), res1)
+        np.testing.assert_array_equal(self.pred_a1a2.matches(), res1)
+        pd.testing.assert_series_equal(self.pred_s1s2.matches(), pd.Series(res1))
 
         res2 = np.array([True, True, False])
-        np.testing.assert_array_equal(self.pred_l1l3.matches, res2)
-        np.testing.assert_array_equal(self.pred_a1a3.matches, res2)
-        pd.testing.assert_series_equal(self.pred_s1s3.matches, pd.Series(res2))
+        np.testing.assert_array_equal(self.pred_l1l3.matches(), res2)
+        np.testing.assert_array_equal(self.pred_a1a3.matches(), res2)
+        pd.testing.assert_series_equal(self.pred_s1s3.matches(), pd.Series(res2))
 
     def test_accuracy(self):
         """Test if accuracy is computed correctly."""
@@ -79,18 +78,91 @@ class TestPrediction(TestCase):
         self.assertEqual(self.pred_a1a3.percentage_correctly_classified, 2 / 3)
         self.assertEqual(self.pred_s1s3.percentage_correctly_classified, 2 / 3)
 
+        # Test alias
+        self.assertEqual(self.pred_l1l3.pcc, 2 / 3)
+        self.assertEqual(self.pred_a1a3.pcc, 2 / 3)
+        self.assertEqual(self.pred_s1s3.pcc, 2 / 3)
+
+
+class TestNumericPrediction(TestCase):
+    """Class to test numeric predictions."""
+
+    l1 = [1, 2, 3]
+    l2 = [1, 2, 3]
+    l3 = [1, 2, 4]
+    l4 = [1, 2, 4, 5]
+    l5 = ["a", "b"]
+    pred_l1l2 = NumericPrediction(l1, l2)
+    pred_l1l3 = NumericPrediction(l1, l3)
+
+    s1 = pd.Series(l1)
+    s2 = pd.Series(l2)
+    s3 = pd.Series(l3)
+    s4 = pd.Series(l4)
+    pred_s1s2 = NumericPrediction(s1, s2)
+    pred_s1s3 = NumericPrediction(s1, s3)
+
+    a1 = np.array(l1)
+    a2 = np.array(l2)
+    a3 = np.array(l3)
+    a4 = np.array(l4)
+    pred_a1a2 = NumericPrediction(a1, a2)
+    pred_a1a3 = NumericPrediction(a1, a3)
+
     def test_residuals(self):
         """Test if residuals are computed correctly"""
-        # Check that error is raised correctly when real_values is not provided
-        p1 = Prediction(self.l1, None)
-        self.assertRaises(ValueError, lambda x: x.residuals, p1)
-        # Check that error is raised correctly when real_values is non-numeric
-        p2 = Prediction(self.l5, self.l5)
-        self.assertRaises(TypeError, lambda x: x.residuals, p2)
+        # Basic residuals
+        p1 = NumericPrediction([1, 2, 3], [1, 2, 3])
+        np.testing.assert_array_equal(p1.residuals(), np.array([0, 0, 0]))
 
-        # Check actual residuals
-        p1.real_values = self.l1
-        np.testing.assert_array_equal(p1.residuals, np.array([0, 0, 0]))
+        np.testing.assert_array_equal(self.pred_l1l3.residuals(), np.array([0, 0, 1]))
+        pd.testing.assert_series_equal(self.pred_s1s3.residuals(), pd.Series([0, 0, 1]))
 
-        np.testing.assert_array_equal(self.pred_l1l3.residuals, np.array([0, 0, 1]))
-        pd.testing.assert_series_equal(self.pred_s1s3.residuals, pd.Series([0, 0, 1]))
+        # Check various parameters
+        p2 = NumericPrediction([1, 1, 1], [3, -3, 3])
+        np.testing.assert_array_equal(p2.residuals(), np.array([2, -4, 2]))
+        # Squared
+        np.testing.assert_array_equal(p2.residuals(squared=True), np.array([4, 16, 4]))
+        # Absolute values
+        np.testing.assert_array_equal(
+            p2.residuals(absolute_value=True), np.array([2, 4, 2])
+        )
+
+    def test_matches_with_tolerance(self):
+        """Test if match with tolerance works."""
+        # Check various parameters
+        p2 = NumericPrediction([1, 1, 1], [3, -3, 4])
+        np.testing.assert_array_equal(
+            p2.matches_tolerance(), np.array([False, False, False])
+        )
+
+        np.testing.assert_array_equal(
+            p2.matches_tolerance(tolerance=2), np.array([True, False, False])
+        )
+
+        np.testing.assert_array_equal(
+            p2.matches_tolerance(tolerance=3), np.array([True, False, True])
+        )
+
+        np.testing.assert_array_equal(
+            p2.matches_tolerance(tolerance=10), np.array([True, True, True])
+        )
+
+
+class TestBinaryPrediction(TestCase):
+    df = pd.read_excel("tests/test_data/binary.xlsx")
+    p1 = BinaryPrediction(df["Fitted"], df["Real"], value_positive=1)
+
+    def test_value_negative(self):
+        self.assertEqual(self.p1.value_negative, 0)
+
+    def test_confusion_matrix(self):
+        real = self.p1.confusion_matrix()
+        exp = np.array([[308, 30], [31, 131]])
+        np.testing.assert_array_equal(real, exp)
+
+    def test_rates(self):
+        self.assertEqual(self.p1.false_positive_rate, (30 / (30 + 308)))
+        self.assertEqual(self.p1.false_negative_rate, (31 / (31 + 131)))
+        self.assertEqual(self.p1.sensitivity, (131 / (31 + 131)))
+        self.assertEqual(self.p1.specificity, (308 / (30 + 308)))

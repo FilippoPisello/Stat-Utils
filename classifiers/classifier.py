994a31e72ad05d7738a41687445b4a7aa77554e3
Filippo Pisello <filippo.pisello@live.it>
2021-11-17 19:50:17 +0100

    refactor: begin change to array-based structure

--- classifiers/classifier.py
+++ classifiers/classifier.py
@@ -5,34 +5,9 @@ import pandas as pd
 
 
 class Classifier:
-    def __init__(
-        self,
-        dataframe: pd.DataFrame,
-        classifier_col: str,
-        usecols: list[str] = None,
-    ):
-        self.df_all = dataframe
-        self.class_col = classifier_col
-        self.data_columns = self._identify_data_columns(usecols)
-        # Predictors and outcomes as numpy arrays
-        self.data = self.df_all.loc[:, self.data_columns].to_numpy()
-        self.outcomes = self._preprocess_outcomes()
-
-    def _identify_data_columns(
-        self, passed_columns: Union[list[str], None]
-    ) -> list[str]:
-        """Return the correct data columns by excluding the classifier column
-        if passed_columns is not provided."""
-        if passed_columns is not None:
-            passed_columns.sort()
-            return passed_columns
-        output = self.df_all.columns.to_list()
-        output.remove(self.class_col)
-        output.sort()
-        return output
-
-    def _preprocess_outcomes(self) -> np.ndarray:
-        return self.df_all[self.class_col].to_numpy().reshape(self.data.shape[0], 1)
+    def __init__(self, predictors: np.array, outcomes: np.array):
+        self.predictors = predictors
+        self.outcomes = outcomes
 
     @property
     def categories(self) -> np.ndarray:
@@ -60,3 +35,29 @@ class Classifier:
     def number_vars(self) -> int:
         """Return the number of variables in the data frame."""
         return self.df_all.shape[1]
+
+    @classmethod
+    def from_dataframe(
+        cls,
+        dataframe: pd.DataFrame,
+        classifier_col: str,
+        usecols: list[str] = None,
+    ):
+        if usecols is None:
+            usecols = dataframe.columns.to_list()
+            usecols.remove(classifier_col)
+
+        outcomes = dataframe[classifier_col].to_numpy()
+
+    def _identify_data_columns(
+        self, passed_columns: Union[list[str], None]
+    ) -> list[str]:
+        """Return the correct data columns by excluding the classifier column
+        if passed_columns is not provided."""
+        if passed_columns is not None:
+            passed_columns.sort()
+            return passed_columns
+        output = self.df_all.columns.to_list()
+        output.remove(self.class_col)
+        output.sort()
+        return output
